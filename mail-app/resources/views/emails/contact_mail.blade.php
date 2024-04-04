b0b9e878be0b82d2bbf3228f283445b6e7f951cd
kishanthini-29 <kishanthini331@gmail.com>
2024-04-04 22:32:14 +0530

    simple Mailing System

---
+++ mail-app/resources/views/emails/contact_mail.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <thead>Name</thead><td>{{$contact_data['name']}}</td>
+
+        </tr>
+        <tr>
+            <thead>Email</thead><td>{{$contact_data['email']}}</td>
+
+        </tr>
+        <tr>
+            <thead>Message</thead><td>{{$contact_data['message']}}</td>
+
+        </tr>
+    </table>
+</body>
+
+</html>
